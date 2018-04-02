04cdec07a8423a88fd72b4749fcb5b3b7b24dc7d
A. Alfonso F. R <alffore@gmail.com>
2018-04-01 19:31:15 -0500

    Se agrega la generación de JSON on metada de la Foto

--- rasp/captura.py
+++ rasp/captura.py
@@ -3,10 +3,23 @@
 ##
 ## AAFR 29 de marzo de 2018
 
+import time
+import json
 from time import sleep
 from picamera import PiCamera
 
 dir_base = '/mnt/lokros/imagenes/'
+url_base = '//127.0.0.1:3000/imagenes/'
+
+
+def guarda_json(datosf):
+    """
+    :type datosf: Diccionario con datos de la foto
+    """
+    with open(dir_base + datosf['nimg'] + '.json', 'w') as outfile:
+        json.dump(datosf, outfile, ensure_ascii=False)
+    return
+
 
 camera = PiCamera()
 
@@ -18,10 +31,13 @@ camera.start_preview()
 # Camera warm-up time
 sleep(2)
 
-id_foto = 1
 while True:
+    datosf = dict()
+    id_foto = int(time.time())
     camera.capture(dir_base + 'lokro' + str(id_foto) + '.jpg', resize=(320, 240))
-    id_foto += 1
-    if id_foto > 700:
-        id_foto = 1
+    datosf['id'] = id_foto
+    datosf['imagen_url'] = url_base + 'lokro' + str(id_foto) + '.jpg'
+    datosf['nimg'] = 'lokro' + str(id_foto)
+    datosf['clasificado'] = ''
+    guarda_json(datosf)
     sleep(60)
