985f74d36ae6e8c14e0af1c4f19d069e92739687
A. Alfonso F. R <alffore@gmail.com>
2019-01-06 20:52:15 -0600

    Se ccrea el lector y entrenado para una solución binaria

---
+++ tf/clasificador2/lector_entrenador_2.py
@@ -0,0 +1,123 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+from tqdm import tqdm
+import random
+import filetype
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import TensorBoard
+import time
+
+DATADIR = "/media/alfonso/COMPARTIDA/devel/Tensorflow/imagenesLokro/imagenes_clas/"
+
+CATEGORIAS = ['dormido', 'despierto']
+
+IMG_SIZE = 70
+
+'''for category in CATEGORIAS:
+    path = os.path.join(DATADIR, category)
+    for img in os.listdir(path):
+        tipo_archivo = filetype.guess(os.path.join(path, img))
+        if tipo_archivo is not None:
+            print(tipo_archivo.mime)
+
+        if tipo_archivo is not None and tipo_archivo.mime == 'image/jpeg':
+            img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+            plt.imshow(img_array, cmap='gray')
+            plt.show()
+            break
+    break
+
+
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array)
+plt.show() '''
+
+
+training_data = []
+
+
+def create_training_data():
+    for category in CATEGORIAS:  # do dogs and cats
+
+        path = os.path.join(DATADIR, category)  # create path to dogs and cats
+        class_num = CATEGORIAS.index(category)  # get the classification  (0 or a 1). 0=dormido 1=despierto
+
+        for img in tqdm(os.listdir(path)):
+            tipo_archivo = filetype.guess(os.path.join(path, img))
+            if tipo_archivo is not None and tipo_archivo.mime == 'image/jpeg':
+                try:
+                    img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)  # convert to array
+                    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
+                    training_data.append([new_array, class_num])  # add this to our training_data
+                except Exception as e:  # in the interest in keeping the output clean...
+                    pass
+
+
+create_training_data()
+
+print("Tamaño del conjunto de entrenamiento: "+str(len(training_data)))
+
+random.shuffle(training_data)
+
+X = []
+y = []
+
+for features,label in training_data:
+    X.append(features)
+    y.append(label)
+
+
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+X = X / 255.0
+
+dense_layers = [0]
+layer_sizes = [128]
+conv_layers = [3]
+
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+
+            NAME = "bz16-adam-c20-{}-conv-{}-nodes-{}-dense-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
+            print(NAME)
+
+            model = Sequential()
+
+            model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:]))
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            for l in range(conv_layer - 1):
+                model.add(Conv2D(layer_size, (3, 3)))
+                model.add(Activation('relu'))
+                model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            model.add(Flatten())
+
+        for _ in range(dense_layer):
+            model.add(Dense(layer_size))
+            model.add(Activation('relu'))
+
+        model.add(Dense(1))
+        model.add(Activation('sigmoid'))
+
+        tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+        model.compile(loss='binary_crossentropy',
+                      optimizer='adam',
+                      # optimizer='rmsprop',
+                      metrics=['accuracy'],
+                      )
+
+        model.fit(X, y,
+                  batch_size=16,
+                  epochs=20,
+                  validation_split=0.3,
+                  callbacks=[tensorboard])
+
+model.save("modeloLokro.h5")
