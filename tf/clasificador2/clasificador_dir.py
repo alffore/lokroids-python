0cfee359afb1c983e4402c9bbd64e078a84eccf4
A. Alfonso F. R <alffore@gmail.com>
2019-01-16 20:19:06 -0600

    Se modifica el codigo del clasificador de directorios de imagenes

--- tf/clasificador2/clasificador_dir.py
+++ tf/clasificador2/clasificador_dir.py
@@ -8,10 +8,10 @@ import filetype
 
 CATEGORIAS = ['dormido', 'despierto', 'otro']
 
+IMG_SIZE = int(sys.argv[2])
+
 
 def preparaimg(filepath):
-    IMG_SIZE = 240
-    # IMG_SIZE = 70
     img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
     new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
     return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
